f72f5e4ad742841ddf2221d3a0334d554d102e3f
dcollier <dj@djcollier.com>
2018-10-05 17:13:57 -0400

    Reorder css stylesheets

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import './index.css';
-import './themes/halloween.css';
 import 'antd/dist/antd.css';
 import 'slick-carousel/slick/slick.css'; 
 import 'slick-carousel/slick/slick-theme.css';
@@ -14,6 +12,7 @@ import 'react-select/dist/react-select.css';
 import 'react-virtualized/styles.css';
 import 'react-virtualized-select/styles.css';
 import './index.css';
+import './themes/halloween.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
